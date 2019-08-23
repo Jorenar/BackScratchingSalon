dd77246e78fdef801ef772fd75b0b086e8fb7fc9
Jorengarenar <jorengarenar@protonmail.com>
2019-08-23 16:31:27 +0200

    Redo customer class as function

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var mouseIsDown = 0;
-var backRows = 13;
 var backColumns = 12;
-var audio = new Audio('sound.ogg');
-var customer;
+var backRows = 13;
+
+var mouseIsDown = 0;
+var mute = 0;
 var pause = 0;
+
 var money = 0;
+
+var satisfy = 0;
+
+var time;
+var timer;
+
+var audio = new Audio('sound.ogg');
 
 var customerDiv = document.querySelector('.customer')
 var moneyDiv = document.getElementById('money').firstChild;
@@ -19,10 +27,13 @@ function whenMouseUp() {
   });
 }
 
-function finalize(satisfy, timeLeft) {
+function finalize(timeLeft) {
   document.querySelector('.back').innerHTML = '';
-  clearInterval(customer.timer)
+  clearInterval(timer)
   timerDiv.innerHTML = '--';
+  if (satisfy > 100)
+    satisfy = 100;
+
   satisfyBar.style.width = satisfy + '%';
 
   money += timeLeft > 0 ? satisfy*timeLeft/100 : Math.floor(satisfy/10);
@@ -34,7 +45,7 @@ function finalize(satisfy, timeLeft) {
 
   setTimeout(() => {
     customerDiv.innerHTML = "";
-    customer = new Customer;
+    createCustomer();
   }, 100);
 }
 
@@ -45,108 +56,98 @@ function createBodyPart(classes, parent) {
   return part;
 }
 
-class Customer {
-  constructor() {
-    let spotRowsArray = new Array(backRows).fill(0);
-    let spotColumnsArray = new Array(backColumns).fill(0);
-    this.verticalScratches = 0;
-    this.horizontalScratches = 0;
-    this.skewScratches = 0;
+function createCustomer() {
+  satisfy = 0;
 
-    this.satisfy = 0;
-    satisfyBar.style.width = '0%';
+  let spotRowsArray = new Array(backRows).fill(0);
+  let spotColumnsArray = new Array(backColumns).fill(0);
+  let verticalScratches = 0;
+  let horizontalScratches = 0;
+  let skewScratches = 0;
 
-    let verticalPoints = Math.floor(Math.random() * 7) + 3;
-    let horizontalPoints = Math.floor(Math.random() * 7) + 3;
-    let skewPoints = Math.floor(Math.random() * 7) + 3;
+  satisfyBar.style.width = '0%';
 
-    this.time = Math.floor((Math.random() * 5) + 20) - Math.max(verticalPoints, horizontalPoints, skewPoints);
+  let verticalPoints = Math.floor(Math.random() * 7) + 3;
+  let horizontalPoints = Math.floor(Math.random() * 7) + 3;
+  let skewPoints = Math.floor(Math.random() * 7) + 3;
 
-    timerDiv.innerHTML = this.time;
+  time = Math.floor((Math.random() * 5) + 20) - Math.max(verticalPoints, horizontalPoints, skewPoints);
 
-    this.timer = setInterval(() => {
-      if (this.time === 0) {
-        finalize(customer.satisfy, 0);
-      } else if (!pause) {
-        timerDiv.innerHTML = --this.time >= 10 ? this.time : '0' + this.time;
-      }
-    }, 1000);
+  timerDiv.innerHTML = time;
 
-    customerDiv.style = "--skin-color: #" + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
+  timer = setInterval(() => {
+    if (time === 0) {
+      finalize(0);
+    } else if (!pause) {
+      timerDiv.innerHTML = --time >= 10 ? time : '0' + time;
+    }
+  }, 1000);
 
-    let head = createBodyPart('head', customerDiv);
-    createBodyPart('neck', customerDiv);
+  customerDiv.style = "--skin-color: #" + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
 
-    let back = createBodyPart('back', customerDiv);
+  let head = createBodyPart('head', customerDiv);
+  createBodyPart('neck', customerDiv);
 
-    let hair = createBodyPart('hair', head);
+  let back = createBodyPart('back', customerDiv);
 
-    if (Math.floor((Math.random() * 2)))
-      hair.classList.add('long')
+  let hair = createBodyPart('hair', head);
 
-    createBodyPart('ears', customerDiv);
+  if (Math.floor((Math.random() * 2)))
+    hair.classList.add('long')
 
-    for (let i = 0; i < backRows; ++i) {
-      for (let j = 0; j < backColumns; ++j) {
-        let spot = document.createElement('div');
-        spot.className = "spot";
-        spot.dataset.row = i;
-        spot.dataset.column = j;
-        spot.onmouseover = () => {
-          if (mouseIsDown) {
-            spot.classList.add('scratched');
+  createBodyPart('ears', customerDiv);
 
-            let scratched = document.querySelectorAll('.scratched');
+  for (let i = 0; i < backRows; ++i) {
+    for (let j = 0; j < backColumns; ++j) {
+      let spot = document.createElement('div');
+      spot.className = "spot";
+      spot.dataset.row = i;
+      spot.dataset.column = j;
+      spot.onmouseover = () => {
+        if (mouseIsDown) {
+          spot.classList.add('scratched');
 
-            if (!mute)
-              audio.play();
+          let scratched = document.querySelectorAll('.scratched');
 
-            if (scratched.length >= 10) {
-              scratched.forEach(spotScratched => {
-                spotRowsArray[spotScratched.dataset.row]++;
-                spotColumnsArray[spotScratched.dataset.column]++;
-              });
+          if (!mute)
+            audio.play();
 
-              let horizontality = spotRowsArray.reduce((n, x) => n + (x === 0), 0);
-              let verticality = spotColumnsArray.reduce((n, x) => n + (x === 0), 0);
+          if (scratched.length >= 10) {
+            scratched.forEach(spotScratched => {
+              spotRowsArray[spotScratched.dataset.row]++;
+              spotColumnsArray[spotScratched.dataset.column]++;
+            });
 
-              let verticalScratch = verticality > horizontality + 2 ? 1 : 0;
-              let horizontalScratch = verticality + 2 < horizontality ? 1 : 0;
-              let skewScratch = Math.abs(verticality - horizontality) < 2 ? 1 : 0;
+            let horizontality = spotRowsArray.reduce((n, x) => n + (x === 0), 0);
+            let verticality = spotColumnsArray.reduce((n, x) => n + (x === 0), 0);
 
-              spotRowsArray.fill(0);
-              spotColumnsArray.fill(0);
+            let verticalScratch = verticality > horizontality + 2 ? 1 : 0;
+            let horizontalScratch = verticality + 2 < horizontality ? 1 : 0;
+            let skewScratch = Math.abs(verticality - horizontality) < 2 ? 1 : 0;
 
-              scratched.forEach(spotScratched => {
-                spotScratched.classList.remove('scratched')
-              });
+            spotRowsArray.fill(0);
+            spotColumnsArray.fill(0);
 
-              if (verticalScratch)
-                this.satisfy += verticalPoints;
-              else if (horizontalScratch)
-                this.satisfy += horizontalPoints;
-              else if (skewScratch)
-                this.satisfy += skewPoints;
+            scratched.forEach(spotScratched => {
+              spotScratched.classList.remove('scratched')
+            });
 
-              if (this.satisfy < 100)
-                document.getElementById("satisfyBar").style.width = this.satisfy + '%';
-              else
-                finalize(100, this.time)
-            }
+            if (verticalScratch)
+              satisfy += verticalPoints;
+            else if (horizontalScratch)
+              satisfy += horizontalPoints;
+            else if (skewScratch)
+              satisfy += skewPoints;
+
+            if (satisfy < 100)
+              document.getElementById("satisfyBar").style.width = satisfy + '%';
+            else
+              finalize(time)
           }
         }
-        back.appendChild(spot);
       }
+      back.appendChild(spot);
     }
-
-    document.querySelectorAll('.unpleasant, .unpleasant *').forEach(item => {
-      item.onmousemove = () => {
-        if (mouseIsDown) {
-          alert("Auch!");
-        }
-      }
-    });
-
   }
 }
 
@@ -154,7 +155,7 @@ function startGame() {
   document.getElementById('playScreen').style.display = '';
   document.getElementById('titleScreen').style.display = 'none';
   document.title = "$" + money + " | Back Scratching Salon"
-  customer = new Customer();
+  createCustomer();
 }
 
 function pauseGame() {
@@ -184,3 +185,5 @@ function toggleMute() {
 }
 
 startGame();
+
+// vim: fen:
